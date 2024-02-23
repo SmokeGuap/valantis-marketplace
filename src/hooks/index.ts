4533428c2cc57499084d9269f2482b34f94ec96d
Kirill <mustang.horse34@gmail.com>
2024-02-23 22:04:13 +0700

    add MainPage

---
+++ src/hooks/index.ts
@@ -0,0 +1,4 @@
+export { default as usePagination } from './usePagination';
+export { default as useHorizontalScroll } from './useHorizontalScroll';
+export { default as useScrollBlock } from './useScrollBlock';
+export { default as useDebounce } from './useDebounce';
